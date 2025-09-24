4b7834ef892375ca31f2cb843b68861f87e727c3
BL4Z3@HYPERYTE.io <lahiru.r@hyperbyte.io>
2025-09-24 08:58:55 +0530

    add links to section

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,15 +1,24 @@
 export default function SideMenu() {
   return (
-    <aside className="sticky pt-14 flex-1 h-full">
+    <aside className="sticky top-0 pt-14 flex-1 h-full">
       <div>
         <p className="mb-4 *:text-lg font-semibold">We have,</p>
         <ul className="ml-4 flex flex-col items-start justify-start gap-3 text-md">
-          <li>Place to stay for trips</li>
-          <li>Air Conditioned Rooms</li>
-          <li>Non Air Conditioned Rooms</li>
-          <li>Breakfast</li>
-          <li>Lunch</li>
-          <li>Dinner</li>
+          <li>
+            <a href="#section-1">Place to stay for trips</a>
+          </li>
+          <li>
+            <a href="#section-2">Air Conditioned Rooms</a>
+          </li>
+          <li>
+            <a href="#section-3">Non Air Conditioned Rooms</a>
+          </li>
+          <li>
+            <a href="#section-4">Breakfast</a>
+          </li>
+          <li>
+            <a href="#section-5">Lunch</a>
+          </li>
         </ul>
       </div>
     </aside>
