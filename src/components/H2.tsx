69ac70ea974ecf2a9eec99b5e9bfd99182a9788a
BL4Z3@HYPERYTE.io <lahiru.r@hyperbyte.io>
2025-09-24 08:58:43 +0530

    increased font size

--- src/components/H2.tsx
+++ src/components/H2.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from "react";
 
 export default function H2(props: PropsWithChildren<{ classes?: string }>) {
   return (
-    <h2 className={`mb-6 text-xl font-semibold ${props.classes}`}>
+    <h2 className={`mb-6 text-2xl font-semibold ${props.classes}`}>
       {props.children}
     </h2>
   );
